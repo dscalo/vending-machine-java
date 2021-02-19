a52561b4a24fc7855ba8bf4676124ee5e8b1dcb1
dansbood <dansbrood@live.com>
2021-02-18 20:04:11 -0500

    adding snack menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void snacksDisplay(ArrayList<Snack> snacks){
+        System.out.println("Welcome to Ashley and Amanda's Snack Shop");
         int index = 1;
         for (Snack snack : snacks) {
             /*
@@ -18,20 +19,43 @@ public class Main {
         System.out.print(" Choose a snack or press q to quit ");
     }
 
+    public static void snackDisplay(Snack snack) {
+        System.out.printf("%s $%.2f\n", snack.getName(), snack.getPrice());
+        System.out.println(snack.getDesc());
+    }
+
     public static void main(String[] args) {
 	// write your code here
         ArrayList<Snack> snacks = Snacks.getSnacks("snacks.csv");
-        System.out.println("Welcome to Ashley and Amanda's Snack Shop");
-        snacksDisplay(snacks);
         Scanner scanner = new Scanner (System.in);
-        String choice = scanner.nextLine();
 
-       try {
-           int num = Integer.parseInt(choice);
+        while(true) {
+            snacksDisplay(snacks);
 
-       } catch (Exception e){
+            String choice = scanner.nextLine();
+            choice = choice.replace("\n", "");
 
-       }
+            if (choice.equals("q")  || choice.equals("Q") ) {
+                System.out.println("Exiting!, have a nice day");
+                break;
+            }
+            try {
+                int snackIndex = Integer.parseInt(choice) -1;
+
+                if (snackIndex < 0 || snackIndex >= snacks.size()) {
+                    continue;
+                }
+
+                Snack snack = snacks.get(snackIndex);
+                snackDisplay(snack);
+
+
+
+            } catch (Exception e) {
+                System.out.println("Invalid selection, please choose a snack or press q to quit");
+            }
+
+        }
 
     }
 }
